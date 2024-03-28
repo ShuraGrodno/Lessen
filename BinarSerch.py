6575872f7c9dd30bcb217a69fd43de4a021fa017
ShuraGrodno <114291448+ShuraGrodno@users.noreply.github.com>
2024-03-28 21:57:24 +0100

    Начало создания тела программы

--- BinarSerch.py
+++ BinarSerch.py
@@ -3,3 +3,12 @@ from tkinter import *
 from tkinter import ttk
 from tkinter.massagbox import askyesno
 from tkinter.filedialog import askopenfilename
+
+class main():
+    def __init__(self):
+        self.root = Tk()
+        self.root.title('Бинарный поиск')
+        
+
+if __name__==__main__:
+    main()

b94d9f8bd646f737f6551df4581573067017ad95
ShuraGrodno <114291448+ShuraGrodno@users.noreply.github.com>
2024-03-28 23:11:00 +0100

    update BinarSerch.py

--- BinarSerch.py
+++ BinarSerch.py
@@ -8,7 +8,8 @@ class main():
     def __init__(self):
         self.root = Tk()
         self.root.title('Бинарный поиск')
+        self.form_1 = Form(self.root)
         
 
 if __name__==__main__:
-    main()
+    BinarSerch = main()

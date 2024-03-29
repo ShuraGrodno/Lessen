d9cfff162b220271c85b34539804eab77c354c79
ShuraGrodno <114291448+ShuraGrodno@users.noreply.github.com>
2024-03-29 12:09:04 +0100

    Создание Фрейма

--- BinarSerch.py
+++ BinarSerch.py
@@ -8,7 +8,13 @@ class main():
     def __init__(self):
         self.root = Tk()
         self.root.title('Бинарный поиск')
-        self.form_1 = Form(self.root)
+        self.frameSerchList = Frame(borderwidth = 2,    #Толщина граници фрейма
+                                    relief = RAISED,    #Определяет тип граници (SUNKEN, REISED, GROOVE, RIDGE)
+                                    cursor = 'clock'    #Вид курсора при навидении на фрейм
+                                    height =            #Высата фрейма
+                                    width =             #Ширина фрейма
+                                    padding = [10]      #Отступы от границ фрейма (может быть от 1 до 4 значений [10, 20, 15, 20])
+                                    )
         
 
 if __name__==__main__:
